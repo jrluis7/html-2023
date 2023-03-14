972e24e265e2c1d2475a661c392379259008e7b1
Luis <jrluis7@gmail.com>
2023-03-14 12:10:15 +0100

    Clase 49

---
+++ Clase40/parts/navegador.php
@@ -0,0 +1,6 @@
+<nav>
+    <a href="index.php"> HOME </a>
+    <a href="pagina_dos.php"> Página 2 </a>
+    <a href="pagina_tres.php"> Página 3 </a>
+    <a href="pagina_cuatro.php"> Página 4 </a>
+</nav>
